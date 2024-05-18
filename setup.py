23eee2064b986ba4ac11c21a9e510550458e95b2
Antonnifo <antonnifo@live.com>
2024-05-18 14:48:21 +0300

    Add setup.py with package configuration for onfon-sms-sender

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='onfon-sms-sender',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'Django'
+    ],
+    entry_points={
+        'console_scripts': [
+            # Add command-line scripts here if needed
+        ],
+    },
+    include_package_data=True,
+    description='A package to send SMS using Onfon Media API',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/antonnifo/Onfon-SMS-Sender',
+    author='Antonnifo',
+    author_email='antonnifo@live.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
