a1b4d508b44f917db19f66c116170c2bf92746c6
Antonnifo <antonnifo@live.com>
2024-05-19 03:37:23 +0300

    add better error handling

--- sms_sender/utils.py
+++ sms_sender/utils.py
@@ -1,20 +1,71 @@
 import json
 import requests
-from django.conf import settings
 import time
+from dotenv import load_dotenv
+import os
+
+try:
+    from django.conf import settings
+except ImportError:
+    pass
+
+load_dotenv()  # Load environment variables from .env file
 
 
 def send_sms(recipients, message):
-    api_url = settings.SMS_API_URL
-    client_id = settings.SMS_CLIENT_ID
-    api_key = settings.SMS_API_KEY
-    sender_id = settings.SMS_SENDER_ID
+    """
+    Sends SMS messages to a list of recipients using the Onfon Media API.
+
+    The function first checks for environment variables. If they are not found,
+    it falls back to using settings from the Django settings module.
+
+    Args:
+        recipients (list of str): List of phone numbers to send the message to.
+        message (str): The message to be sent.
+
+    Returns:
+        list of dict: A list of responses from the API for each batch of messages sent.
+                      Each response contains the API's response or an error message.
+
+    Raises:
+        ValueError: If the required configuration settings are missing.
+    """
+    api_url = os.getenv('SMS_API_URL', getattr(settings, 'SMS_API_URL', None))
+    client_id = os.getenv('SMS_CLIENT_ID', getattr(
+        settings, 'SMS_CLIENT_ID', None))
+    api_key = os.getenv('SMS_API_KEY', getattr(settings, 'SMS_API_KEY', None))
+    sender_id = os.getenv('SMS_SENDER_ID', getattr(
+        settings, 'SMS_SENDER_ID', None))
+
+    missing_settings = []
+    if not api_url:
+        missing_settings.append('SMS_API_URL')
+    if not client_id:
+        missing_settings.append('SMS_CLIENT_ID')
+    if not api_key:
+        missing_settings.append('SMS_API_KEY')
+    if not sender_id:
+        missing_settings.append('SMS_SENDER_ID')
+
+    if missing_settings:
+        raise ValueError(f"Missing required SMS configuration settings: {\
+                         ', '.join(missing_settings)}")
+
     max_numbers_per_packet = 20
     max_requests_per_second = 40
 
     headers = {'Content-Type': 'application/json'}
 
     def send_request(batch):
+        """
+        Sends a batch of SMS messages to the API.
+
+        Args:
+            batch (list of str): List of phone numbers in the current batch.
+
+        Returns:
+            dict: The API's response or an error message.
+        """
         data = {
             "SenderId": sender_id,
             "MessageParameters": [
